4f0532a664b6b18f2fd6248c8da4b3e405fda6b6
Junlapong L <junlapong@gmail.com>
2019-05-14 02:01:09 +0700

    use go modules

--- go/main.go
+++ go/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 	"todo/task"
 
 	echo "github.com/labstack/echo/v4"
@@ -22,5 +26,21 @@ func main() {
 
 	e.Static("/static", "static")
 
-	e.Logger.Fatal(e.Start(":1323"))
+	// Start server
+	go func() {
+		if err := e.Start(":1323"); err != nil {
+			e.Logger.Info("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server
+	// with a timeout of 10 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
